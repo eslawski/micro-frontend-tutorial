a0b37832e7d80575228a38a6d9b0d25ffc73d027
Evan Slawski <evan.slawski@ithaka.org>
2021-02-15 16:24:30 -0500

    Introduce shared modules and add asyncronously loading to fixup remote apps

--- products/src/index.js
+++ products/src/index.js
@@ -1,10 +1,5 @@
-import faker from 'faker';
-
-let products = "";
-
-for (let i = 0; i < 3; i++) {
-    const name = faker.commerce.productName();
-    products += `<div>${name}</div>`
-}
-
-document.querySelector("#dev-products").innerHTML = products;
+// Importing in this way gives webpack the opportunity to asyncronously load modules
+// in the rest of our source code. This fixes the "Shared module is not available for eager
+// consumption" error we get when attempting to load this in isolation with our shared
+// dependencies as configured in our ModuleFederationPlugin.
+import("./bootstrap");
