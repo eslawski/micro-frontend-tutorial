cb9914efe763db3d03d12ae435d1bd7d90d188f2
Evan Slawski <evan.slawski@ithaka.org>
2021-02-18 18:25:49 -0500

    Put marketing inside of container

--- part-2/container/src/App.js
+++ part-2/container/src/App.js
@@ -1,5 +1,9 @@
 import React from "react";
+import MarketingApp from "./components/MarketingApp";
 
 export default () => {
-    return <h1>Hi there!</h1>;
+    return <div>
+        <h1>You are in the Container App</h1>
+        <MarketingApp />
+    </div>
 };
