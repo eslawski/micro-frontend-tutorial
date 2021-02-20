efee3e3f96ad23cefe0e72dcab72d4340ed712e6
Evan Slawski <evan.slawski@ithaka.org>
2021-02-20 12:14:12 -0500

    simple change

--- part-2/container/src/App.js
+++ part-2/container/src/App.js
@@ -3,7 +3,7 @@ import MarketingApp from "./components/MarketingApp";
 
 export default () => {
     return <div>
-        <h1>You are in the Container App!</h1>
+        <h1>You are in the Container App</h1>
         <MarketingApp />
     </div>
 };
