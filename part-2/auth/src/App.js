9b1ef320137e2df4957471d137011b936ba75a3d
Evan Slawski <evan.slawski@ithaka.org>
2021-02-23 19:11:21 -0500

    Fix public path routing to work in all dev scenarios

--- part-2/auth/src/App.js
+++ part-2/auth/src/App.js
@@ -2,8 +2,8 @@ import React from "react";
 import { Switch, Route, Router} from "react-router-dom";
 import { StylesProvider, createGenerateClassName } from "@material-ui/core/styles";
 
-import Signin from "./components/Signin";
-import Signup from "./components/Signup";
+import SignIn from "./components/Signin";
+import SignUp from "./components/Signup";
 
 const generateClassName = createGenerateClassName({
     productionPrefix: "au"
@@ -14,8 +14,8 @@ export default ({ history }) => {
         <StylesProvider generateClassName={generateClassName}>
             <Router history={history}>
                 <Switch>
-                    <Route path="auth/signin" component={Signin} />
-                    <Route path="auth/signup" component={Signup} />
+                    <Route path="/auth/signin" component={SignIn} />
+                    <Route path="/auth/signup" component={SignUp} />
                 </Switch>
             </Router>
         </StylesProvider>
